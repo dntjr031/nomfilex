376262b00156c13caee3854d75508d87cef1de2e
wooseok <dntjr031@gmail.com>
2021-07-21 16:31:32 +0900

    3일차 완료

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const header = () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="/tv">TV</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="/search">Search</a>
+      </li>
+    </ul>
+  </header>
+);
+
+export default header;
